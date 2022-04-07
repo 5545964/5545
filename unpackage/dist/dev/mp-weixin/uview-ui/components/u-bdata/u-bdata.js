38b341a73714668861aea0659950caddbac1e847
jiangtao <1093635664@qq.com>
2022-04-07 17:11:11 +0800

    江涛

---
+++ unpackage/dist/dev/mp-weixin/uview-ui/components/u-bdata/u-bdata.js
@@ -0,0 +1,11 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["uview-ui/components/u-bdata/u-bdata"],{886:function(n,t,e){"use strict";e.r(t);var r=e(887),u=e(889);for(var a in u)"default"!==a&&function(n){e.d(t,n,(function(){return u[n]}))}(a);e(891);var i,c=e(32),o=Object(c["default"])(u["default"],r["render"],r["staticRenderFns"],!1,null,"2f5907aa",null,!1,r["components"],i);o.options.__file="uview-ui/components/u-bdata/u-bdata.vue",t["default"]=o.exports},887:function(n,t,e){"use strict";e.r(t);var r=e(888);e.d(t,"render",(function(){return r["render"]})),e.d(t,"staticRenderFns",(function(){return r["staticRenderFns"]})),e.d(t,"recyclableRender",(function(){return r["recyclableRender"]})),e.d(t,"components",(function(){return r["components"]}))},888:function(n,t,e){"use strict";var r;e.r(t),e.d(t,"render",(function(){return u})),e.d(t,"staticRenderFns",(function(){return i})),e.d(t,"recyclableRender",(function(){return a})),e.d(t,"components",(function(){return r}));var u=function(){var n=this,t=n.$createElement;n._self._c},a=!1,i=[];u._withStripped=!0},889:function(n,t,e){"use strict";e.r(t);var r=e(890),u=e.n(r);for(var a in r)"default"!==a&&function(n){e.d(t,n,(function(){return r[n]}))}(a);t["default"]=u.a},890:function(n,t,e){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var r={name:"u-adata",props:{list:{type:Array}},data:function(){return{imgPath:this.$imgPath}},methods:{xuanzhong:function(n){this.$emit("click",n)}}};t.default=r},891:function(n,t,e){"use strict";e.r(t);var r=e(892),u=e.n(r);for(var a in r)"default"!==a&&function(n){e.d(t,n,(function(){return r[n]}))}(a);t["default"]=u.a},892:function(n,t,e){}}]);
+//# sourceMappingURL=../../../../.sourcemap/mp-weixin/uview-ui/components/u-bdata/u-bdata.js.map
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'uview-ui/components/u-bdata/u-bdata-create-component',
+    {
+        'uview-ui/components/u-bdata/u-bdata-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('1')['createComponent'](__webpack_require__(886))
+        })
+    },
+    [['uview-ui/components/u-bdata/u-bdata-create-component']]
+]);
